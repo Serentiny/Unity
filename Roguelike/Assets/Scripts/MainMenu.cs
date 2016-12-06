9c4533978ad2547cd1265a8ca33fcedeffc1fd5f
Serentiny <dimaszav@gmail.com>
2016-12-06 20:53:52 +0300

    Script Update

--- Roguelike/Assets/Scripts/MainMenu.cs
+++ Roguelike/Assets/Scripts/MainMenu.cs
@@ -2,22 +2,62 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-    public Button startButton;
-    public Button quitButton;
+public class MainMenu : MonoBehaviour
+{
+    public Canvas canvasLogo, canvasMainMenu, canvasOption, canvasScore;
+    public Button resumeOn, resumeOff;
 
-	void Start ()
+    bool isPlayerAlive;
+
+	void Start()
     {
-        startButton = startButton.GetComponent<Button> ();
-        quitButton  =  quitButton.GetComponent<Button> ();
+        isPlayerAlive = false;  //Check saves
+
+        resumeOn.gameObject.SetActive(isPlayerAlive);
+        resumeOff.gameObject.SetActive(!isPlayerAlive);
+
+        canvasLogo.gameObject.SetActive(true);
+        canvasMainMenu.gameObject.SetActive(false);
+        canvasOption.gameObject.SetActive(false);
+        canvasScore.gameObject.SetActive(false);
     }
 
-    public void StartPress()
+    void Update()
+    {
+        if (canvasLogo.gameObject.activeSelf && Time.realtimeSinceStartup > 2.0f)
+        {
+            canvasLogo.gameObject.SetActive(false);
+            canvasMainMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void GameNew()
     {
         SceneManager.LoadScene("Dungeon");
     }
-
-    public void QuitPress()
+    public void GameResume()
+    { }
+    public void OptionsShow()
+    {
+        canvasMainMenu.gameObject.SetActive(false);
+        canvasOption.gameObject.SetActive(true);
+    }
+    public void OptionsHide()
+    {
+        canvasOption.gameObject.SetActive(false);
+        canvasMainMenu.gameObject.SetActive(true);
+    }
+    public void ScoresShow()
+    {
+        canvasMainMenu.gameObject.SetActive(false);
+        canvasScore.gameObject.SetActive(true);
+    }
+    public void ScoresHide()
+    {
+        canvasScore.gameObject.SetActive(false);
+        canvasMainMenu.gameObject.SetActive(true);
+    }
+    public void Quit()
     {
         Application.Quit();
     }
